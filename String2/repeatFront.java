a9b180f90c75dc911d9fadd3846da3042194ed67
MouMajhi23 <144060531+MouMajhi23@users.noreply.github.com>
2023-09-05 00:54:24 +0530

    Update repeatFront.java

--- String2/repeatFront.java
+++ String2/repeatFront.java
@@ -1,1 +1,12 @@
-
+public String repeatFront(String str, int n) {
+  //created a temp String , substring is used to trim the string 
+  String temp = str.substring(0,n);
+  String res="";
+  
+  for(int i =0 ; i<n ;i++){
+    res = res + temp;
+    temp = temp.substring(0,n-i-1);
+    
+  }
+  return res;
+}
